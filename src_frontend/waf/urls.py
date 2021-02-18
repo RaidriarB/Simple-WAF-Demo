96f82e4a3934c8cb5465e55a67dcfddac91245fd
JohenanLi <599192367@qq.com>
2021-02-18 22:09:47 +0800

    基本解决，外键查找有点小问题

--- src_frontend/waf/urls.py
+++ src_frontend/waf/urls.py
@@ -1,15 +1,20 @@
-from django.urls import path
+from django.urls import path,re_path
 
 from . import views
 
 urlpatterns = [
 	path('index',views.IndexView.as_view(),name='index'),
     path('log',views.LogView.as_view(),name='log'),
+    re_path(r'^log_del-(?P<nid>\d+)/', views.log_del),
+    re_path(r'^log_detail-(?P<nid>\d+)/', views.log_detail),
     path('table',views.LogView.as_view(),name='rule'),
     path('form',views.FormView.as_view(),name='form'),
     path('ui-elements',views.UIElementsView.as_view(),name='ui-elements'),
     path('chart',views.ChartView.as_view(),name='chart'),
     path('empty',views.EmptyView.as_view(),name='empty'),
     path('tab-panel',views.TabPanelView.as_view(),name='tab-panel'),
-    path('rule',views.RuleView.as_view(),name= 'rule')
+    path('rule',views.RuleView.as_view(),name= 'rule'),
+    re_path(r'^rule_del-(?P<nid>\d+)/', views.rule_del),
+    re_path(r'^rule_edit-(?P<nid>\d+)/', views.rule_edit),
+    path('rule_create',views.rule_create)
 ]
--- src_frontend/waf/views.py
+++ src_frontend/waf/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.views import generic
 
-from .models import Log,Rule
-
+from .models import Log,Rule,Fulllog
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.shortcuts import redirect
 
 class IndexView(generic.TemplateView):
     template_name = 'waf/index.html'
@@ -12,7 +14,7 @@ class LogView(generic.ListView):
     context_object_name = 'latest_log_list'
 
     def get_queryset(self):
-        return Log.objects.order_by('time')[:5]
+        return Log.objects.order_by('time')[:20]
 
 class RuleView(generic.ListView):
     template_name = 'waf/rule.html'
@@ -35,3 +37,39 @@ class TabPanelView(generic.TemplateView):
 
 class UIElementsView(generic.TemplateView):
     template_name = 'waf/ui-elements.html'
+
+
+def rule_del(request,nid):  #删除
+    Rule.objects.filter(id=nid).delete()
+    return redirect("rule")
+
+def rule_edit(request,nid):  #修改
+    if request.method=="GET":
+        obj=Rule.objects.filter(id=nid).first()
+        return render(request, 'waf/rule_edit.html', {"obj": obj})
+    elif request.method=="POST":
+        contentGet=request.POST.get("content")      #拿到提交的数据
+        descriptionGet=request.POST.get("description")
+        actionGet = request.POST.get("action")
+        Rule.objects.filter(id=nid).update(content = contentGet, description = descriptionGet, action = actionGet)
+        return redirect("rule")
+
+def rule_create(request):
+    if request.method=="GET":
+        return render(request, 'waf/rule_create.html')
+    elif request.method=="POST":
+        contentGet=request.POST.get("content")      #拿到提交的数据
+        descriptionGet=request.POST.get("description")
+        actionGet = request.POST.get("action")
+        Rule.objects.create(content = contentGet, description = descriptionGet, action = actionGet)
+        return redirect("rule")
+
+def log_detail(request,nid):
+    logGet = Log.objects.filter(id=nid).first()
+    FulllogGet = Fulllog.objects.get(log = logGet)
+    print(FulllogGet)
+    return render(request, 'waf/log_detail.html', {"log": logGet,'Fulllog':FulllogGet})
+
+def log_del(request,nid):  #删除
+    Log.objects.filter(id=nid).delete()
+    return redirect("log")
